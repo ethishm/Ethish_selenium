f9c4e151f1f788fc3c9e3391cdb8554bf8c068f6
Ethish M <ethishm@ami.com>
2021-08-12 16:09:46 +0530

    test cases modified

--- Config/config.py
+++ Config/config.py
@@ -1,9 +1,11 @@
+import os
+
 class Testdata:
 
     Base_url = "https://letskodeit.com/"
-
+    DIR = os.getcwd()
     Username = "test@email.com"
     Password = "abcabc"
 
-    CHROME_EXECUTABLE_PATH = "/Users/ethishm/Desktop/Drivers/chromedriver.exe"
-    FIREFOX_EXECUTABLE_PATH = "/Users/ethishm/Desktop/Drivers/geckodriver.exe"
+    CHROME_EXECUTABLE_PATH = r"{}\Drivers\chromedriver.exe".format(DIR)
+    FIREFOX_EXECUTABLE_PATH = r"{}\Drivers\geckodriver.exe".format(DIR)
